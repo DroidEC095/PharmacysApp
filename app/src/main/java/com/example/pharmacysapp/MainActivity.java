816d981410c57de3cdf782d37f7b277f377d4e37
droidec095 <xdanilo_y095@hotmail.com>
2022-05-16 20:13:40 -0500

    git commit

---
+++ app/src/main/java/com/example/pharmacysapp/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.pharmacysapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
